4a07708fbea2a42ecd7899782ed38fa344426512
Sithik ALIYAR <saliyar@pc-lheea186.ec-nantes.fr>
2021-02-06 10:52:53 +0100

    Corrections for Volume estimation update SPAR

--- FloatingSPAR/Estimation_of_UWVolume.m
+++ FloatingSPAR/Estimation_of_UWVolume.m
@@ -13,11 +13,16 @@ D_spar=0.45;
 D_tower=0.28;
 Coning_height=0.2;
 L_Spar=1.95;
+Mass_entered=340.8;
 
 
 %% VaryingInput
 Overall_Draft=2.285;
-OF_Volume= 0.3;
+OF_alphafieldVolume=3.6114104e+01;
+Domain_Length=2.7;
+Domain_width=2.7;
+Domain_Waterdepth=5;
+Density=1000;
 
 
 %% Solid Volume calucaltion
@@ -31,7 +36,7 @@ if (Diff_height>0.2)
 
     %In any case SPAR will not come out of water - So its underwater volume is fixed 
    
-    Vol_cone= 1/3 * pi * Coning_height * (r^2+r*R+R^2);
+    Vol_cone= 1/3 * pi * Coning_height * (r_tower^2+r_tower*r_spar+r_spar^2);
     h_tower=Diff_height-0.2;
     Vol_tower = pi/4 * D_tower^2 *h_tower;
     Total_Volume= Vol_cone+Vol_SPAR+Vol_tower;
@@ -42,15 +47,47 @@ else
     Total_Volume= Vol_cone+Vol_SPAR;
 end
 
-fprintf('Theoratical Total submerged volume is....(cu.m)',Total_Volume);
+disp('********************Volume Comparision**********************')
+
+fprintf('Theoratical Total submerged volume is %f(cu.m) \n',Total_Volume);
+
+OF_Volume=  (Domain_Length*Domain_width*Domain_Waterdepth)-OF_alphafieldVolume;
+
+
 
 Diff_Volume=Total_Volume-OF_Volume;
 
-fprintf('Difference in  Total submerged volume in foamstar is....(cu.m)',Diff_Volume);
 
-Diff_draft=(Diff_Volume/(pi/4 * D_tower^2))/100;
+fprintf('Difference in  Total submerged volume in foamstar is %f (cu.m) \n',Diff_Volume);
 
-fprintf('Difference in draft in Tower is....(cm)',Diff_draft);
+
+Percentage_Volumeloss= (Diff_Volume/Total_Volume)*100;
+
+fprintf('The percentage loss in Volume from OF and Theory is %f  \n \n',Percentage_Volumeloss);
+
+disp('*****************Mass comparision***********************')
+
+Diff_mass= Diff_Volume*Density;
+
+Theoratical_mass=Total_Volume*Density;
+
+fprintf('Theoratical Total mass is %f (kg) \n',Theoratical_mass);
+
+fprintf('foamstar input, mass entered in the dict is %f (kg) \n',Mass_entered);
+
+OF_mass=OF_Volume*Density;
+
+fprintf('OF Total mass is %f (kg) \n',OF_mass);
+
+fprintf('Difference in  Total  mass in foamstar is %f (kg) \n \n',Diff_mass);
+
+
+disp('*****************Draft comparision***********************')
+
+
+Diff_draft=(Diff_Volume/(pi/4 * D_tower^2))*100;
+
+fprintf('Difference in draft in Tower is %f (cm) \n',Diff_draft);
 
 
 
